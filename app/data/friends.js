f58b03d274eb203e6e2d87df83d07c104e2d3cfe
joshua dodd <joshuadodd@joshuas-MacBook-Pro.local>
2017-08-11 11:48:30 -0400

    checkpoint

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,8 +1,8 @@
 // Data structure that will store the friends data
 var friendsArray = [
 	{
-		name: 'Larry',
-		photo: 'https://static.pexels.com/photos/50582/selfie-monkey-self-portrait-macaca-nigra-50582.jpeg',
+		name: 'Charlie',
+		photo: 'https://s-media-cache-ak0.pinimg.com/originals/16/5c/4c/165c4cdc621205711be67cf054842414.jpg',
 		scores: [
 			'1',
 			'5',
@@ -15,33 +15,151 @@ var friendsArray = [
 			'1',
 			'5'
 		]
-    },
-{
-		name: 'Larry',
-		photo: 'https://static.pexels.com/photos/50582/selfie-monkey-self-portrait-macaca-nigra-50582.jpeg',
+	},
+	{
+		name: 'Oscar',
+		photo: 'https://s-media-cache-ak0.pinimg.com/236x/71/d7/90/71d790b6dc2192b70f110eddf6bbd5a3.jpg',
+		scores: [
+			'2',
+			'4',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Ralph',
+		photo: 'https://i.ytimg.com/vi/UEzqkyD8t0Y/hqdefault.jpg',
+		scores: [
+			'4',
+			'3',
+			'5',
+			'1',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Bubba',
+		photo: 'https://i.ytimg.com/vi/QbJ0d2oJ-rI/hqdefault.jpg',
+		scores: [
+			'5',
+			'1',
+			'2',
+			'1',
+			'3',
+			'4',
+			'1',
+			'3',
+			'3',
+			'4'
+		]
+	},
+	{
+		name: 'Sir Jenkins',
+		photo: 'http://thumbpress.com/wp-content/uploads/2014/07/Crazy-Animals-from-Past-4.jpg',
 		scores: [
 			'1',
-			'5',
-			'5',
 			'1',
 			'5',
 			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'1'
+		]
+	},
+	{
+		name: 'Clarence',
+		photo: 'http://1.bp.blogspot.com/-W6lw1Yj2J8E/Tx2mfUe4ZNI/AAAAAAAADco/tX5KSPMcNBY/s640/funny-animals-making-funny-faces-015.jpg',
+		scores: [
 			'1',
+			'1',
+			'5',
+			'1',
+			'3',
+			'2',
+			'5',
 			'1',
 			'1',
 			'5'
 		]
-    },
+	},
+	{
+		name: 'Bennidict',
+		photo: 'http://listverse.wpengine.netdna-cdn.com/wp-content/uploads/2016/05/10-grizzly-autumn_000017839911_Small.jpg',
+		scores: [
+			'5',
+			'4',
+			'4',
+			'2',
+			'4',
+			'4',
+			'3',
+			'2',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: 'Bob Marley',
+		photo: 'http://www.bajiroo.com/wp-content/uploads/2013/03/crazy-funny-monkey-animals-pictures-photos-images-bajiroo-humor-blog-pics-6.jpg',
+		scores: [
+			'5',
+			'2',
+			'3',
+			'1',
+			'5',
+			'1',
+			'3',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'McCheesin',
+		photo: 'https://i.pinimg.com/236x/bd/cc/3f/bdcc3f1051497ec019000ff624bf8ee8--laughing-animals-smiling-animals.jpg',
+		scores: [
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5'
+		]
+	},
+	{
+		name: 'Flappy',
+		photo: 'https://i.pinimg.com/736x/f2/b9/a8/f2b9a8225c53617c273ff4c2cad1e9b0--crazy-animals-funny-animals.jpg',
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	}
+];
 
-
-
-
-
-
-
-
-
-]
-    
-
-    module.exports = friendsArray;
+module.exports = friendsArray; 	
