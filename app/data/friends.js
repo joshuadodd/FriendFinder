f1f76d29403144c3b4765551d4236e132e46a5bf
joshua dodd <joshuadodd@lawn-128-61-61-143.lawn.gatech.edu>
2017-08-10 20:56:22 -0400

    setup

---
+++ app/data/friends.js
@@ -0,0 +1,47 @@
+// Data structure that will store the friends data
+var friendsArray = [
+	{
+		name: 'Larry',
+		photo: 'https://static.pexels.com/photos/50582/selfie-monkey-self-portrait-macaca-nigra-50582.jpeg',
+		scores: [
+			'1',
+			'5',
+			'5',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1',
+			'5'
+		]
+    },
+{
+		name: 'Larry',
+		photo: 'https://static.pexels.com/photos/50582/selfie-monkey-self-portrait-macaca-nigra-50582.jpeg',
+		scores: [
+			'1',
+			'5',
+			'5',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1',
+			'5'
+		]
+    },
+
+
+
+
+
+
+
+
+
+]
+    
+
+    module.exports = friendsArray;
